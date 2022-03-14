271fe8a93659ae94b59df1a0b5605124c86e329c
izabayoleonce <izabayoleonce@gmail.com>
2022-03-14 14:30:20 +0100

    Creatuion de l'objet teams

--- model/Teams.php
+++ model/Teams.php
@@ -5,6 +5,69 @@ use PDO;
 
 class Teams 
 {
+    private $_id,
+            $_logo,
+            $_nomEquipe;
+    
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+    
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) 
+        {
+            $method ='set'.ucfirst($key);
+            if (method_exists($this, $method)) 
+            {
+                $this->$method($value);
+            }
+
+        }
+    }
+
+    public function setId($id)
+    {
+        $id = (int) $id;
+
+        if ($id > 0) 
+        {
+            $this->_id = $id;
+        }
+    }
+
+    public function setLogo($logo)
+    {
+        if (is_string($logo)) {
+            $this->_logo =$logo;
+        }
+    }
+    
+    public function setNomEquipe($nomEquipe)
+    {
+        if (is_string($nomEquipe)) {
+            $this->_nomEquipe =$nomEquipe;
+        }
+    }
+    
+
+    public function getId()
+    {
+      return $this->_id;
+    }
+
+    public function getLogo()
+    {
+      return $this->_logo;
+    }
+
+    public function getNomEquipe()
+    {
+      return $this->_nomEquipe;
+    }
+
+    
 
 } 
 ?>
